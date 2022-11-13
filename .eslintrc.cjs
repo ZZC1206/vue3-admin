5fd3e6f15e068d0726e97576243633748dcb91d8
ZZC1206 <960052730@qq.com>
2022-11-13 14:15:22 +0800

    chore: 完成EsLint配置的修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: ['./tsconfig.eslint.json']
   },
   plugins: [
     'vue'
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置选项
+      cache: false // 禁用 eslint 缓存
+    }),
+    vueJsx({
+      // 配置选项
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }
 })
