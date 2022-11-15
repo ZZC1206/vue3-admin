96f0673cfe8fe8b402767722fadcd6b5fd51d3b7
ZZC1206 <960052730@qq.com>
2022-11-15 12:31:52 +0800

    chore: 完善配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -45,20 +45,25 @@ export default defineConfig({
         additionalData: '@import "@/styles/variables.scss";'
       }
     }
+  },
+  server: {
+    open: false, // 编译完成后是否自动打开网页
+    host: '0.0.0.0', // 指定使用地址，默认 localhost， 0.0.0.0 代表可以被外界访问
+    port: 8080 // 指定访问的端口号
+    // proxy: { // 代理跨域
+    //   // 字符串简写写法
+    //   '/foo': 'http://localhost:4567', // => 'http://localhost:4567/foo'
+    //   // 选项写法
+    //   '/api': {
+    //     // 代理的目标地址
+    //     target: 'http://jsonplaceholder.typicode.com',
+    //     // 是否允许跨域，true，代理服务会把 origin 修改为目标地址 http://jsonplaceholder.typicode.com
+    //     changeOrigin: true,
+    //     ws: false, // （选）是否启用 websockets
+    //     secure: false, // （选）是否启用 https 接口
+    //     // （选）路径重写，匹配 /api 的字符串，替换换成空字符串
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
   }
-  // server: {
-  //   proxy: {
-  //     // 字符串简写写法
-  //     '/foo': 'http://localhost:4567', // => 'http://localhost:4567/foo'
-  //     // 选项写法
-  //     '/api': {
-  //       // 代理的目标地址
-  //       target: 'http://jsonplaceholder.typicode.com',
-  //       // true，代理服务会把 origin 修改为目标地址 http://jsonplaceholder.typicode.com
-  //       changeOrigin: true,
-  //       // 路径重写
-  //       rewrite: (path) => path.replace(/^\/api/, '')
-  //     }
-  //   }
-  // }
 })
